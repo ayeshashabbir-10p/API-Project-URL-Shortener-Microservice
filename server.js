378442cc98c2e8dba3e3997796f05188e74bf3be
Ayesha Shabbir <ayeshashabbir@tenpearls.com>
2021-01-27 16:24:18 +0500

    first commit

--- server.js
+++ server.js
@@ -1,32 +1,116 @@
-// server.js
-// where your node app starts
+'use strict';
 
-// init project
 var express = require('express');
+var MongoClient = require("mongodb").MongoClient;
+var ObjectId = require("mongodb").ObjectId;
+var mongoose = require('mongoose');
+var shortId = require('shortid');
+var bodyParser = require('body-parser');
+var validUrl = require('valid-url');
+require('dotenv').config();
+var cors = require('cors');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// Basic Configuration 
+var port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
+app.use(cors());
+app.use(express.json());
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+const uri = process.env.MONGO_URI;
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000 // Timeout after 5s instead of 30s
 });
 
+const connection = mongoose.connection;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+//Create Schema
+const Schema = mongoose.Schema;
+const urlSchema = new Schema({
+  original_url: String,
+  short_url: String
+})
+const URL = mongoose.model("URL", urlSchema);
 
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+
+
+app.post('/api/shorturl/new', async function (req, res) {
+
+  const url = req.body.url_input
+  const urlCode = shortId.generate()
+
+  // check if the url is valid or not
+  if (!validUrl.isWebUri(url)) {
+    res.status(401).json({
+      error: 'invalid URL'
+    })
+  } else {
+    try {
+      // check if its already in the database
+      let findOne = await URL.findOne({
+        original_url: url
+      })
+      if (findOne) {
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        })
+      } else {
+        // if its not exist yet then create new one and response with the result
+        findOne = new URL({
+          original_url: url,
+          short_url: urlCode
+        })
+        await findOne.save()
+        res.json({
+          original_url: findOne.original_url,
+          short_url: findOne.short_url
+        })
+      }
+    } catch (err) {
+      console.error(err)
+      res.status(500).json('Server erorr...')
+    }
+  }
+})
+
+
+app.get('/api/shorturl/:short_url?', async function (req, res) {
+  try {
+    const urlParams = await URL.findOne({
+      short_url: req.params.short_url
+    })
+    if (urlParams) {
+      return res.redirect(urlParams.original_url)
+    } else {
+      return res.status(404).json('No URL found')
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json('Server error')
+  }
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port : ${port}`);
+})
+
+
+
